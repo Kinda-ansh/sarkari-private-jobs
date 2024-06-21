dd40c2cac4f224c0c4db625d38c7004b4414b9ef
Anshuman Tiwari <anshumantiwari.cse@gmail.com>
2024-06-21 15:12:07 +0530

    updated instance

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://devapi.sarkariprivatejobs.in:8080/api/v1",
+  baseURL: "https://dev.sarkariprivatejobs.in/api/v1",
 });
 // https://msmev2.navoditfinserv.in/index.php/api
-export default instance;
+export default instance;    
